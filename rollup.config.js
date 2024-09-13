d3d0838a843a1fc4bea9205a9372eba0ce1f1493
Youssef Hussein <youssefalcon30@gmail.com>
2024-09-13 21:37:25 +0300

    updts

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.ts", // Entry point
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.es.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    terser(),
+  ],
+  external: ["react", "react-dom"], // Mark peer dependencies as external
+};
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,10 +1,70 @@
-import styled from "styled-components";
+import * as React from "react";
+import { cn } from "./lib/cn";
+import { Slot, Slottable } from "@radix-ui/react-slot";
 
-export const Button = styled.button`
-  background-color: #4caf50;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
+import { cva, VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "group inline-flex items-center justify-center rounded whitespace-nowrap font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-300 active:scale-[0.99] duration-500 hover:scale-[1.02]",
+  {
+    variants: {
+      variant: {
+        default: "bg-zinc-900 text-zinc-50 dark:text-zinc-900 dark:bg-zinc-50",
+        destructive:
+          "bg-red-500 text-zinc-50 dark:bg-red-900 dark:text-zinc-50",
+        outline:
+          "border border-zinc-300 bg-white dark:border-zinc-800 dark:bg-zinc-950 hover:dark:bg-zinc-800 hover:bg-zinc-200",
+        secondary:
+          "bg-zinc-200 text-zinc-900 dark:bg-zinc-800 dark:text-zinc-50",
+        ghost: "hover:bg-zinc-200 dark:hover:bg-zinc-700",
+        link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
+      },
+      size: {
+        default: "h-10 px-5 py-2",
+        sm: "h-9 px-3",
+        lg: "h-12 px-8 text-lg",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  rightIcon?: React.ReactNode;
+  leftIcon?: React.ReactNode;
+  hideIcon?: boolean;
+}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+
+      ...props
+    },
+    ref
+  ) => {
+    const Component = asChild ? Slot : "button";
+    return (
+      <Component
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        <Slottable>{props.children}</Slottable>
+      </Component>
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
